131a2f26e2f8dc1640b54ec2acc1fa445c7c1516
Amin Subagiyo <aminsum9@gmail.com>
2023-01-24 00:55:49 +0700

    update ui

--- lib/pages/edituser.dart
+++ lib/pages/edituser.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:pretty_json/pretty_json.dart';
-import 'home.dart';
 import '../config/url.dart' as host;
+import '../styles/colors.dart' as colors;
 
 class EditUser extends StatefulWidget {
   final List list;
@@ -61,11 +60,21 @@ class EditUserState extends State<EditUser> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Edit Data User"),
-        backgroundColor: Colors.lightGreen,
+        title: const Text(
+          "Edit Data User",
+          style: TextStyle(color: colors.SECONDARY_COLOR),
+        ),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: colors.SECONDARY_COLOR),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        shadowColor: Colors.transparent,
       ),
       body: Container(
-          padding: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(10.0),
           child: Card(
               child: Container(
             padding: const EdgeInsets.all(10.0),
@@ -73,9 +82,6 @@ class EditUserState extends State<EditUser> {
               Column(
                 children: [
                   const Padding(padding: EdgeInsets.all(10.0)),
-                  const Text("Edit Data User",
-                      style: TextStyle(
-                          fontSize: 25.0, fontWeight: FontWeight.bold)),
                   const Padding(padding: EdgeInsets.all(10.0)),
                   const Padding(padding: EdgeInsets.all(10.0)),
                   TextField(
@@ -103,16 +109,25 @@ class EditUserState extends State<EditUser> {
                   const Padding(padding: EdgeInsets.all(10.0)),
                 ],
               ),
-              TextButton(
-                  onPressed: () {
-                    editDataUser();
-                  },
-                  child: Text("EDIT", style: TextStyle(color: Colors.white)),
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.lightGreen,
-                  )),
             ]),
           ))),
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: TextButton(
+            onPressed: () {
+              editDataUser();
+            },
+            style: TextButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              backgroundColor: colors.PRIMARY_COLOR,
+            ),
+            child: const Text("EDIT", style: TextStyle(color: Colors.white)),
+          ),
+        ),
+      ),
     );
   }
 }
